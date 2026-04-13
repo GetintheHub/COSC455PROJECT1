991fe96d358263a364d6fdfaa623b273593ad928
GetintheHub <kamilstew1@gmail.com>
2026-04-13 01:45:34 -0400

    Created the syntax tree and incorporated the lexer into main

---
+++ main.rs
@@ -0,0 +1,40 @@
+mod ast;
+mod lexer;
+mod token;
+
+use std::env;
+use std::fs;
+use std::process;
+
+use lexer::Lexer;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("lolcompiler <input.lol>");
+        process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    if !filename.ends_with(".lol") {
+        eprintln!(" input file must have a .lol extension");
+        process::exit(1);
+    }
+
+    let source = match fs::read_to_string(filename) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("could not read '{}': {}", filename, e);
+            process::exit(1);
+        }
+    };
+
+    let mut lexer = Lexer::new(&source);
+    let tokens = lexer.tokenize_all();
+
+    for token in tokens {
+        println!("{:?} => {:?}", token.kind, token.lexeme);
+    }
+}
---
+++ syntaxtree.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Clone)]
+pub enum Node {
+    Document(Vec<Node>),
+    Head(Vec<Node>),
+    Title(String),
+    Paragraph(Vec<Node>),
+    Bold(String),
+    Italics(String),
+    List(Vec<Node>),
+    Item(Vec<Node>),
+    Comment(String),
+    Link(String),
+    Newline,
+    Text(String),
+    VarDef {
+        name: String,
+        value: String,
+    },
+    VarUse(String),
+}
